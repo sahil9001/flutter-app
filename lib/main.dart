7209d79e27a0514770842818bb0b3f86b7140c44
Sahil Silare <sahilsilare@gmail.com>
2019-11-23 14:28:47 +0530

    On branch master  Changes to be committed: 	new file:   assets/index.jpeg 	modified:   lib/main.dart 	modified:   pubspec.yaml

--- lib/main.dart
+++ lib/main.dart
@@ -1,66 +1,113 @@
 import 'package:flutter/material.dart';
 
-void main(){
-  runApp(new MyApp());  
-}
+void main() => runApp(new MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
-      title: "Hello World",
-      home: new HomePage(),
-      theme: new ThemeData(
-        primarySwatch: Colors.green,
-        brightness: Brightness.light,
-        accentColor: Colors.green
-      ),
+        home: new LoginPage(),
+        theme: new ThemeData(
+          primarySwatch: Colors.blue,
+          brightness: Brightness.light,
+          accentColor: Colors.blueAccent
+        ),
     );
   }
 }
-class HomePage extends StatefulWidget {
+
+class LoginPage extends StatefulWidget {
   @override
-  _HomePageState createState() => _HomePageState();
+  _LoginPageState createState() => _LoginPageState();
 }
 
-class _HomePageState extends State<HomePage> {
-  String mytext = "Hello World";
-  void _changeText(){
-  setState((){
-    if(mytext.startsWith("H"))
-    mytext = "Clicked";
-    else
-    mytext = "Hello World";
-  });
-}
+class _LoginPageState extends State<LoginPage> with SingleTickerProviderStateMixin{
 
-  Widget _bodyWidget(){
-  return new Container(
-    padding: const EdgeInsets.all(8.0),
-    child: new Center(
-      child: new Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          new Text(mytext, style: new TextStyle(
-            fontSize: 30.0,
-          ),),
+  AnimationController _iconAnimationController;
+  Animation<double> _iconAnimation;
 
-        ],
-      ),
-    ),
-  );
-}
+  @override
+  void initState(){
+    super.initState();
+    _iconAnimationController = new AnimationController(
+      vsync:this,
+      duration: new Duration(milliseconds: 500)
+    );
+    _iconAnimation = new CurvedAnimation(
+      parent: _iconAnimationController,
+      curve: Curves.easeOut
+    );
+    _iconAnimation.addListener(()=> this.setState((){}));
+    _iconAnimationController.forward();
+  }
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-      appBar: new AppBar(
-        title: new Text("Home Page"),
-
-      ),
-      body: _bodyWidget(),
-      floatingActionButton: new FloatingActionButton(
-        child: new Icon(Icons.add),
-        onPressed: _changeText,
+      backgroundColor: Colors.blueAccent,
+      body: new Stack(
+        fit: StackFit.expand,
+        children: <Widget>[
+          new Image(
+            image: new AssetImage("assets/index.jpeg"),
+            fit: BoxFit.cover,
+            color: Colors.black45,
+            colorBlendMode: BlendMode.darken,
+          ),
+          new Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              new FlutterLogo(
+                size: _iconAnimation.value * 100,
+              ),
+              new Form(
+                child: new Theme(
+                  data:new ThemeData(
+                    brightness: Brightness.dark,
+                    primarySwatch: Colors.teal,
+                    inputDecorationTheme: new InputDecorationTheme(
+                      labelStyle: new TextStyle(
+                        color: Colors.white54,
+                        fontSize: 20.0
+                      )
+                    )
+                  ),
+                child: new Container(
+                padding: const EdgeInsets.all(40.0),
+                child: new Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    new TextFormField(
+                      decoration: new InputDecoration(
+                        labelText: "Enter Email",
+                      ),
+                      keyboardType: TextInputType.emailAddress,
+                    ),
+                    new TextFormField(
+                      decoration: new InputDecoration(
+                        labelText: "Enter Password",
+                      ),
+                      keyboardType: TextInputType.text,
+                      obscureText: true,
+                    ),
+                    new Padding(
+                      padding: const EdgeInsets.only(top:40.0),
+                    ),
+                    new MaterialButton(
+                      height: 40.0,
+                      minWidth: 100.0,
+                      color: Colors.teal,
+                      textColor: Colors.white,
+                      child: new Icon(Icons.arrow_right),
+                      onPressed: ()=>{},
+                    )
+                  ],
+                ),
+                )
+                )
+              )
+            ],
+          )
+        ],
       ),
     );
   }
